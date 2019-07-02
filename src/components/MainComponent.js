71f57a7cddf5155c73b5206ad580a70aee510d67
Rohan Sharma <rohansharma251998@gmail.com>
2019-07-02 12:58:14 +0530

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import Home from './HomeComponent' 
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponents';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -43,6 +44,7 @@ class Main extends Component{
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Redirect to="/home" />
           </Switch>
         <Footer/>
--- src/shared/leaders.js
+++ src/shared/leaders.js
@@ -1,8 +1,8 @@
 export const LEADERS = [
     {
       id: 0,
-      name: 'Peter Pan',
-      image: '/assets/images/alberto.png',
+      name: 'Rohan Sharma',
+      image: '/assets/images/1.png',
       designation: 'Chief Epicurious Officer',
       abbr: 'CEO',
       featured: false,
@@ -11,7 +11,7 @@ export const LEADERS = [
     {
       id: 1,
       name: 'Dhanasekaran Witherspoon',
-      image: '/assets/images/alberto.png',
+      image: '/assets/images/2.png',
       designation: 'Chief Food Officer',
       abbr: 'CFO',
         featured: false,
@@ -20,7 +20,7 @@ export const LEADERS = [
     {
       id: 2,
       name: 'Agumbe Tang',
-      image: '/assets/images/alberto.png',
+      image: '/assets/images/3.png',
       designation: 'Chief Taste Officer',
       abbr: 'CTO',
       featured: false,
@@ -28,8 +28,8 @@ export const LEADERS = [
     },
     {
       id: 3,
-      name: 'Alberto Somayya',
-      image: '/assets/images/alberto.png',
+      name: 'Rohan Sharma',
+      image: '/assets/images/1.png',
       designation: 'Executive Chef',
       abbr: 'EC',
       featured: true,
