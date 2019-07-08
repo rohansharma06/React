3382a5d0cddc7cc0bdb28bba7c83dcc6b10fe4b4
Rohan Sharma <rohansharma251998@gmail.com>
2019-07-08 22:46:58 +0530

    Fetch Post Comment

--- src/components/DishdetailComponents.js
+++ src/components/DishdetailComponents.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody, CardText, CardTitle,Breadcrumb,BreadcrumbItem,
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => (val) && (val.length);
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -30,7 +31,7 @@ toggleModal() {
 
 handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 }
 
 render() {
@@ -101,7 +102,7 @@ render() {
         return (
             <div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
@@ -111,7 +112,7 @@ render() {
         );       
     }
 
-    function RenderComments({comments,addComment, dishId}) {
+    function RenderComments({comments,postComment, dishId}) {
         if (comments != null) {
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -133,7 +134,7 @@ render() {
                             );
                         })}                   
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment} />
+                    <CommentForm dishId={dishId} postComment={postComment} />
                </div>
             );
         }
@@ -177,7 +178,7 @@ render() {
                     <div  className="row">
                         <RenderDish dish={props.dish}/>
                         <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id}
                         />
                     </div>
